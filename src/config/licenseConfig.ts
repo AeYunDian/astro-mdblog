49e3fc9a034df4dd7459c18870f2d3d28a7ceda1
AeYunDian <AeYunDian@users.noreply.github.com>
2026-08-05 09:03:23 +0000

    Deploying to global from @ AeYunDian/astro-mdblog@15649e25dbe6ab1fd6005a2d474ce6a82f638438 🚀

--- src/config/licenseConfig.ts
+++
@@ -1,8 +0,0 @@
-import type { LicenseConfig } from "../types/config";
-
-// 文章许可协议配置
-export const licenseConfig: LicenseConfig = {
-  enable: true,
-  name: "CC BY-SA 4.0",
-  url: "https://creativecommons.org/licenses/by-sa/4.0/deed.zh",
-};
